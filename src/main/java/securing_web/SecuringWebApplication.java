6dfd2db45b974b8f62bc05c501b118aa180f2fa2
Khushimishra-3112 <mishra.khushi3112@gmail.com>
2026-07-21 04:00:07 +0530

    feat: implement Spring Security web application with custom login

---
+++ src/main/java/securing_web/SecuringWebApplication.java
@@ -0,0 +1,13 @@
+package securing_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuringWebApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(SecuringWebApplication.class, args);
+	}
+
+}
